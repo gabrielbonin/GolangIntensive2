b8f251a804f1c0d40a143072d68c4c004a0feb34
Gabriel Bonin <gabrielbonin@MacBook-Pro-de-Gabriel.local>
2023-03-14 20:57:00 -0300

    improve chanel comunication with rabbitmq

---
+++ pkg/rabbitmq/rabbitmq.go
@@ -0,0 +1,36 @@
+package rabbitmq
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+func openChannel() (*amqp.Channel, error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		return nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	return ch, nil
+}
+
+func Consume(ch *amqp.Channel, out chan amqp.Delivery) error {
+	msgs, error := ch.Consume(
+		"order",
+		"go-consumer",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if error != nil {
+		return error
+	}
+	for msg := range msgs {
+		out <- msg
+	}
+	return nil
+}
